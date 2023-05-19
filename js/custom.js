106cd56ba3f2f41920238b91aaef0daa9b552cf2
ADWAITH RAJ <117984453+adw-ith@users.noreply.github.com>
2023-05-19 22:22:21 +0530

    Added hamburger menu

--- js/custom.js
+++ js/custom.js
@@ -80,3 +80,77 @@
     });
 
 })(jQuery);
+
+// Wait for the DOM to be ready
+document.addEventListener('DOMContentLoaded', function() {
+	// Add event listener to the form
+	document.querySelector('#formsubmit-view').addEventListener('submit', send_mail);
+});
+
+function send_mail(event) {
+    event.preventDefault(); // Prevent the default form submission
+
+	// Get the values from the form
+	const name = document.querySelector('#name').value;
+	const email = document.querySelector('#email').value;
+	const phoneNo = document.querySelector('#phone').value;
+	const message = document.querySelector('#message').value;
+	const subject = document.querySelector('#subject').value;
+
+	// Check if required fields are not empty
+    if (name.trim() === '' || email.trim() === '' || message.trim() === '') {
+        alert('Please fill in all required fields.');
+        return;
+    }
+
+    // Check if email is valid using a regular expression
+    const emailRegex = /^\S+@\S+\.\S+$/;
+    if (!emailRegex.test(email)) {
+        alert('Please enter a valid email address.');
+        return;
+    }
+
+    // Check if phone number is valid (assuming a 10-digit number)
+    const phoneRegex = /^\d{10}$/;
+    if (!phoneRegex.test(phoneNo)) {
+        alert('Please enter a valid 10-digit phone number.');
+        return;
+    }
+
+	// Send mail using formsubmit.co
+	fetch("https://formsubmit.co/ajax/a04df2ccae5b848911f9d624d1a98820", {
+		method: "POST",
+		headers: {
+			'Content-Type': 'application/json'
+			// 'Accept': 'application/json'
+		},
+		body: JSON.stringify({
+			name: name,
+			email: email,
+			phone: phoneNo,
+			message: message,
+			_subject: subject,
+			_template: "table",
+			_captcha: false
+    	})
+	})
+    .then(response => response.json())
+    .then(data => console.log(data))
+    .catch(error => console.log(error));
+
+	// Hide the form and show the success message
+	document.querySelector('#formsubmit-view').style.display = 'none';
+	document.querySelector('#submitted-view').style.display = 'block';
+}
+
+const hamicon = document.querySelector(".hammenu")
+const menu = document.querySelector(".hamlist")
+
+hamicon.addEventListener("click", () => {
+    if (menu.classList.contains("hamdisp")){
+        menu.classList.remove("hamdisp")
+    }
+    else{
+        menu.classList.add("hamdisp")
+    }
+})
